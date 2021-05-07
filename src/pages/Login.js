737f0554c9e1ce36a685cdf49dcf45c3d3f830a6
akshaypatel99 <akshaypatel99@gmail.com>
2021-05-07 19:55:35 +0100

    Tailwind configuration

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,25 @@
+import { useState, useContext, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import FirebaseContext from '../context/firebase';
+
 const Login = () => {
+	const [emailAddress, setEmailAddress] = useState('');
+	const [password, setPassword] = useState('');
+
+	const [error, setError] = useState('');
+	const isInvalid = password === '' || emailAddress === '';
+
+	const history = useHistory();
+	const { firebase } = useContext(FirebaseContext);
+
+	const loginHandler = () => {};
+
+	useEffect(() => {
+		document.title = 'Login - Instagram';
+	}, []);
+
 	return (
-		<div>
+		<div className='container flex mx-auto max-w-screen-md items-center h-screen'>
 			<p>Login page</p>
 		</div>
 	);
